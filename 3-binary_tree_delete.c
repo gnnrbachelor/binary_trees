73de6c27a87065330464b7fd3108768fe339b80d
gnnrbachelor <gnnrbachelor@gmail.com>
2021-07-21 15:48:59 +0000

    Add 3-binary_tree_delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes a binary tree
+ * @tree: Tree
+ * Return: Void
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
