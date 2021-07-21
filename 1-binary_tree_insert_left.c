bc2439c218a3292e56715fc99a30529bf04f158b
gnnrbachelor <gnnrbachelor@gmail.com>
2021-07-21 14:49:15 +0000

    Fix 1-binary ...

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,12 +21,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	n_node->n = value;
 	n_node->parent = parent;
 	n_node->right = NULL;
+	n_node->left = parent->left;
 	if (parent->left != NULL)
 	{
-		n_node->left = parent->left;
 		n_node->left->parent = n_node;
+		parent->left = n_node;
 
 	}
-	parent->left = n_node;
 	return (n_node);
 }
