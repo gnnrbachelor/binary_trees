0592ff9606fd050ea3a74e7727ed3c515ffa7e4f
gnnrbachelor <gnnrbachelor@gmail.com>
2021-07-22 14:33:57 +0000

    Fix betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left || tree->right)
-		count += (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
+		count += (binary_tree_nodes(tree->left) +
+			  binary_tree_nodes(tree->right) + 1);
 
 	return (count);
 }
