ac997d25d23f236e8bda07558818df50627bc4cd
gnnrbachelor <gnnrbachelor@gmail.com>
2021-07-22 17:04:08 +0000

    Add 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - Checks if tree is perfect
+ * @tree: Tree
+ * Return: 0 or 1
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (binary_tree_is_leaf(tree))
+		return (1);
+
+	if (!tree->left || !tree->right)
+		return (0);
+
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	{
+		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+			return (1);
+	}
+	return (0);
+}
