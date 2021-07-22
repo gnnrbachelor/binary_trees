b2c6ed56cbf7cd1658cf1cc605859dd55d3e136f
gnnrbachelor <gnnrbachelor@gmail.com>
2021-07-22 17:07:40 +0000

    Fixed for header problem

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,19 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - Checks if leaf
+ * @node: Node
+ * Return: 1 or 0
+ */
+
+int binary_tree_is_leaf_1(const binary_tree_t *node)
+{
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
+/**
  * binary_tree_is_perfect - Checks if tree is perfect
  * @tree: Tree
  * Return: 0 or 1
@@ -11,7 +24,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_is_leaf(tree))
+	if (binary_tree_is_leaf_1(tree))
 		return (1);
 
 	if (!tree->left || !tree->right)
--- binary_trees.h
+++ binary_trees.h
@@ -42,4 +42,5 @@ int binary_tree_balance(const binary_tree_t *tree);
 size_t binary_tree_height_1(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
+int binary_tree_is_leaf_1(const binary_tree_t *node);
 #endif
