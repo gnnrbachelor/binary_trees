6e6bc7ab7441070686d0704c0e22f1f8b515e500
gnnrbachelor <gnnrbachelor@gmail.com>
2021-07-22 17:15:21 +0000

    Fix betty error

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -56,7 +56,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (binary_tree_height_2(tree->left) == binary_tree_height_2(tree->right))
 	{
-		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		if (binary_tree_is_perfect(tree->left)
+		   && binary_tree_is_perfect(tree->right))
 			return (1);
 	}
 	return (0);
