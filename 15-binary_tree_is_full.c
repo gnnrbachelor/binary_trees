328e5f4b82bcad3c7e476b1ce1333104f94b9acf
gnnrbachelor <gnnrbachelor@gmail.com>
2021-07-22 15:44:56 +0000

    Fix betty

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,7 +10,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL )
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	else
 		return (binary_tree_is_full(tree->left)
