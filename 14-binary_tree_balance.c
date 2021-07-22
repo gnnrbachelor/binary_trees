c6ce0c5a83ae9f45c899b01a6c69b44e66d8fcbd
gnnrbachelor <gnnrbachelor@gmail.com>
2021-07-22 15:09:46 +0000

    Fix for weird header thing

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,30 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - Measures height of tree
+ * @tree: Tree
+ * Return: height or 0
+ *
+ */
+
+size_t binary_tree_height_1(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if ((tree == NULL) || (tree->left == NULL && tree->right == NULL))
+	{
+		return (0);
+	}
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left > right)
+		return (left + 1);
+	return (right + 1);
+}
+
+/**
  * binary_tree_balance - Returns balance factor
  * @tree: Tree
  * Return: 0 or balance factor
